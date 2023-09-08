497fdf7e8065b04b6b6aedd2094a8a9865d7d442
CamaraHacks <gustavo-camara@outlook.com>
2023-09-08 14:20:19 -0300

    clock and color added

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,9 +6,13 @@ class AlienInvasion:
         def __init__(self):
             """Initialize the game, and create game resources."""
             pygame.init()
+            self.clock = pygame.time.Clock()
             
-            self.screen = pygame.display.set_mode((1200, 800))
+            self.screen = pygame.display.set_mode((960, 400))
             pygame.display.set_caption("Alien Invasion")
+
+            #set the background color 
+            self.bg_color=(230, 230, 230)
         
         def run_game(self):
             """Start the main loop for the game."""
@@ -20,6 +24,7 @@ class AlienInvasion:
 
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
+                self.clock.tick(60)
 if __name__ == '__main__':
     # Make a game instance, and run the game. or something else
     ai = AlienInvasion()
