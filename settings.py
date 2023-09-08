75c8055c1d79cb71768eb985bd0049d76f0ddbd4
CamaraHacks <gustavo-camara@outlook.com>
2023-09-08 14:43:53 -0300

    settings of the alien_invasion game

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    """A class to store all settings for Alien_invasion game"""
+
+    def __init__(self):
+        """Initialize the game settings"""
+        #screen settings
+        self.screen_width = 960
+        self.screen_height = 720
+        self.bg_color = (41, 40, 45)
+
+        
