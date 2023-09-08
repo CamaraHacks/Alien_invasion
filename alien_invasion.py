3362b16231b4482a76fba330f049d95ae85dac7d
CamaraHacks <gustavo-camara@outlook.com>
2023-09-08 14:43:26 -0300

    refactored bkcolor and settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 class AlienInvasion:
         """Overall class to manage game assets and behavior."""
 
@@ -7,12 +8,16 @@ class AlienInvasion:
             """Initialize the game, and create game resources."""
             pygame.init()
             self.clock = pygame.time.Clock()
+            self.settings = Settings()
             
-            self.screen = pygame.display.set_mode((960, 400))
+            self.screen = pygame.display.set_mode(
+                 (self.settings.screen_width, self.settings.screen_height))
+            
+            
             pygame.display.set_caption("Alien Invasion")
 
-            #set the background color 
-            self.bg_color=(230, 230, 230)
+            
+          
         
         def run_game(self):
             """Start the main loop for the game."""
@@ -21,6 +26,9 @@ class AlienInvasion:
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         sys.exit()
+
+                # redraw the screen during each loop
+                self.screen.fill(self.settings.bg_color)
 
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
