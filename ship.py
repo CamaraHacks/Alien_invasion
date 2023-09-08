3ba82ce091c800773e5b20b776222ae9e34f188e
CamaraHacks <gustavo-camara@outlook.com>
2023-09-08 15:14:01 -0300

    Ship pre-set-config for the game

---
+++ ship.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Ship:
+    """A class to handle the ship"""
+
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #load the ship image and gets its rect.
+        self.image = pygame.image.load('images/green_ship.bmp')
+        self.rect = self.image.get_rect()
+
+        #Start each new ship at the bottom ceter of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the shit at its current location"""
+        self.screen.blit(self.image, self.rect) 
