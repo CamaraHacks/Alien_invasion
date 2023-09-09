fb3ae8717e82f4e60cf058002ff1afada4c8f1dc
CamaraHacks <gustavo-camara@outlook.com>
2023-09-09 13:35:01 -0300

    speed increased

--- settings.py
+++ settings.py
@@ -4,8 +4,9 @@ class Settings:
     def __init__(self):
         """Initialize the game settings"""
         #screen settings
-        self.screen_width = 960
-        self.screen_height = 720
-        self.bg_color = (41, 40, 45)
+        self.screen_width = 1366
+        self.screen_height = 688
+        self.bg_color = (41, 40, 50)
 
-        
+        #sets the speed of the ship
+        self.ship_speed = 3
