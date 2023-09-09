88660921394e3c0a75104389818da5a806aa87cc
CamaraHacks <gustavo-camara@outlook.com>
2023-09-09 11:51:55 -0300

    ship presets now have a move feature

--- ship.py
+++ ship.py
@@ -7,13 +7,26 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        #Movent flag; start with a ship that is not moving
+        self.moving_right = False
+        self.moving_left = False
 
         #load the ship image and gets its rect.
-        self.image = pygame.image.load('images/green_ship.bmp')
+        self.image = pygame.image.load('D:\Projects\Alien_invasion\images\green_ship.bmp')
         self.rect = self.image.get_rect()
 
         #Start each new ship at the bottom ceter of the screen
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Movement flag; start with a ship that's not moving.
+        self.moving_right = False
+    
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right:
+            self.rect.x += 5
+        if self.moving_left:
+            self.rect.x -=5
 
     def blitme(self):
         """Draw the shit at its current location"""
