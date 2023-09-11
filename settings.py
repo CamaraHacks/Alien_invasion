f9b301eea8a8ba276cdf401358d263a60ca355ab
CamaraHacks <gustavo-camara@outlook.com>
2023-09-11 05:52:12 -0300

    missiles conf correction

--- settings.py
+++ settings.py
@@ -7,6 +7,13 @@ class Settings:
         self.screen_width = 1366
         self.screen_height = 688
         self.bg_color = (41, 40, 50)
+        
+        #Missiles config
+        self.missile_speed = 2.0
+        self.missile_width = 3
+        self.missile_height = 15
+        self.missile_color = (60, 60, 60)
+        
 
         #sets the speed of the ship
         self.ship_speed = 3
