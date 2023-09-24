4178240c6abb87f24faeac2127a609272b7bb48f
CamaraHacks <gustavo-camara@outlook.com>
2023-09-23 22:31:08 -0300

    alien class

---
+++ alien.py
@@ -0,0 +1,20 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Alien(Sprite):
+    """Alien ships squadron"""
+
+    def __init__(self, ai_game):
+        """Initialize aliens starting pos"""
+        super().__init__()
+        self.screen = ai_game.screen
+
+        #load alien image
+        self.image = pygame.image.load('D:\Projects\Alien_invasion\images\alien.png')
+        self.image = pygame.transform.scale(self.image, (100, 200))
+        self.rect = self.image.get_rect()
+        # Start each new alien near the top left of the screen.
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+        # Store the alien's exact horizontal position.
+        self.x = float(self.rect.x)
