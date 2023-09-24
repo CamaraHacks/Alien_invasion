1a4f9412cc2158476c58f2e047b3f1537ad073f4
CamaraHacks <gustavo-camara@outlook.com>
2023-09-24 13:54:07 -0300

    alien fleet and bkg_image features added

--- alien_invasion.py
+++ alien_invasion.py
@@ -31,9 +31,32 @@ class AlienInvasion:
 
         def _create_fleet(self):
             """create a fleet of aliens"""
-             #make Alien.
+            #fill the screen with alien.
+            #space between aliens
             alien = Alien(self)
-            self.aliens.add(alien)  
+            alien_width, alien_height = alien.rect.size
+
+            current_x, current_y = alien_width, alien_height
+            while current_y < (self.settings.screen_height - (-2)   * alien_height):
+                while current_x < (self.settings.screen_width - (-3) * alien_width):
+                    self._create_alien(current_x,current_y)
+                    current_x += 2 * alien_width
+
+                # Finished a row; reset x value, and increment y value.
+                current_x = alien_width
+                current_y += 3 * alien_height
+
+        def _create_alien(self, x_position, y_position):
+            """add alien rows"""
+            new_alien = Alien(self)
+            new_alien.x = x_position
+            new_alien.rect.x = x_position
+            new_alien.rect.y = y_position
+            self.aliens.add(new_alien)
+            
+
+
+
 
             
           
@@ -97,7 +120,8 @@ class AlienInvasion:
 
         def _update_screen(self):
                 """Update images on the screen and flip to the new screen"""
-                self.screen.fill(self.settings.bg_color)
+                self.screen.blit(self.settings.bkg_image, (0, 0))
+
                 for missile in self.missiles.sprites():
                     missile.draw_missil()
                 self.ship.blitme()
