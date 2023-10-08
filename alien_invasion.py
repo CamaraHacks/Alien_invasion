cba9aeece4fdad044eb29aec9858ca115cf07279
CamaraHacks <Gustavo-camara@outlook.com>
2023-10-08 02:30:38 -0300

    alien edge featured has been added

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,14 +37,14 @@ class AlienInvasion:
             alien_width, alien_height = alien.rect.size
 
             current_x, current_y = alien_width, alien_height
-            while current_y < (self.settings.screen_height - (-2)   * alien_height):
+            while current_y < (self.settings.screen_height - (2)   * alien_height):
                 while current_x < (self.settings.screen_width - (-3) * alien_width):
                     self._create_alien(current_x,current_y)
                     current_x += 2 * alien_width
 
                 # Finished a row; reset x value, and increment y value.
                 current_x = alien_width
-                current_y += 3 * alien_height
+                current_y +=  2 * alien_height
 
         def _create_alien(self, x_position, y_position):
             """add alien rows"""
@@ -132,6 +132,26 @@ class AlienInvasion:
                 self.aliens.draw(self.screen)
                 
                 pygame.display.flip()
+
+
+        def check_edges(self):
+             """Change the direction of aliens"""
+             for alien in self.aliens.sprites():
+                  if alien.check_edges():
+                       self._change_fleet_direction()
+                       break
+        
+        def _change_fleet_direction(self):
+            """drop the entire fleet"""
+            for alien in self.aliens.sprites():
+                alien.rect.y += self.settings.fleet_drop_speed
+            self.settings.fleet_direction *= -1
+
+        def _update_aliens(self):
+            """Check if the fleet is at an edge, then update positions."""
+            self._check_fleet_edges()
+            self.aliens.update()
+
               
 if __name__ == '__main__':
     # Make a game instance, and run the game. or something else
