6e28b8432bec203bcf323d1531a090e67680dd91
CamaraHacks <Gustavo-camara@outlook.com>
2023-10-09 11:54:26 -0300

    alien update and check_edge fixed

--- alien.py
+++ alien.py
@@ -8,13 +8,26 @@ class Alien(Sprite):
         """Initialize aliens starting pos"""
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         #load alien image
-        self.image = pygame.image.load('D:\Projects\Alien_invasion\images\ship_alien.png')
-        self.image = pygame.transform.scale(self.image, (80, 80))
+        self.image = pygame.image.load('images\ship_alien.png')
+        self.image = pygame.transform.scale(self.image, (50, 50))
         self.rect = self.image.get_rect()
         # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
         # Store the alien's exact horizontal position.
         self.x = float(self.rect.x)
+
+    def check_edges(self):
+        """aliens move to the other direction when hit the edge of the galaxy"""
+        screen_rect = self.screen.get_rect()
+        return (self.rect.right >= screen_rect.right) or (self.rect.left <= 0)
+
+    def update(self):
+        """Move alien to the right"""
+        self.x += self.settings.alien_speed * self.settings.fleet_direction
+        self.rect.x = self.x
+
+   
